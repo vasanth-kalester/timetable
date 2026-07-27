aba77cab22e798020439f787dfd625eb430c468b
vasanth-kalester <whyneedtosay@gmail.com>
2026-07-27 14:48:18 +0530

    minor fix

--- web_client/src/app/api/auth/[...nextauth]/route.ts
+++ web_client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcryptjs"
