a299681e0fe3502b445a4c085539017a608cbce1
vasanth-kalester <whyneedtosay@gmail.com>
2026-07-27 18:27:20 +0530

    files cleanup

--- web_client/setup.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-fs.writeFileSync('.env', 'DATABASE_URL="file:./dev.db"\nNEXTAUTH_SECRET="my-super-secret-key-for-local-dev"\nNEXTAUTH_URL="http://localhost:3000"\n', 'utf8');
-console.log('.env file created successfully.');
