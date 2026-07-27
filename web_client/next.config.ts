6c2fc86da12a85109d11ec3dc786669c1a4fbaa4
vasanth-kalester <whyneedtosay@gmail.com>
2026-07-27 14:53:42 +0530

    minor fix -  2

--- web_client/next.config.ts
+++ web_client/next.config.ts
@@ -1,12 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.110"],
   experimental: {
     serverActions: {
       allowedOrigins: ["localhost:3001", "192.168.0.110:3001"],
     },
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
